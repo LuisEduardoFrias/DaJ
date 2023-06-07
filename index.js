95e43525d8b29d37f09532b2d4d5210f3ab38f2d
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2023-06-07 00:05:05 -0400

    update index.js

--- index.js
+++ index.js
@@ -2,262 +2,241 @@ const fs = require("fs");
 const { Guid } = require("js-guid");
 const db_name = "./daj.json";
 
-class daj {}
-
-class Class {
-   constructor(name) {
-      this.class_data_name = name;
+//Data Archive Json
+class daj {
+   constructor() {
       this.key = Guid.newGuid().StringGuid;
    }
 }
 
-function createFileDb(ob) {
+const errors = {
+   notAdd: "not data add",
+   notData: "not data found",
+   callBack: "A 'callback' is require.",
+   key: "'key' value is require.",
+   objectRequite: "'obj' value is require.",
+   isNotObject: "'obj' is not an object",
+};
+
+function createFileDb(callback) {
    if (!fs.existsSync(db_name)) {
-      console.log(`The data file does create`);
+      console.log(`The data file does create.`);
 
       fs.appendFile(db_name, "", (err) => {
          if (err) throw err;
 
-         fs.chmod(db_name, 0o400, (err) => {
+         /* fs.chmod(db_name, 0o400, (err) => {
             if (err) throw err;
             fs.chmod(db_name, 0o200, (err) => {
                if (err) throw err;
+               callback(undefined);
             });
-         });
-      });
+         });*/
 
-      ob.json(undefined);
+         callback(undefined);
+      });
    } else {
-      getAll(ob);
+      getAll(callback);
    }
 }
 
-const error = (res, obj, key) => {
-   if (typeof res === "function") {
-      console.log("getter: firts 'res'(callback) paranme is quired.");
-   }
-
-   if (typeof obj === "string") {
-      console.log("getter: second 'obj'(string) paranme is quired.");
-   }
-
-   if (typeof key === "string") {
-      console.log("getter: trees 'key'(string) paranme is quired.");
-   }
-};
+function argumentsCheck(arg) {
+   const propertys = Reflect.ownKeys(arg);
+   propertys.forEach((e) => {
+      switch (e) {
+         case "callback": {
+            if (typeof Reflect.get(arg, e) !== "function")
+               console.log(errors.callBack);
+            break;
+         }
+         case "key": {
+            if (!e.isEmpty()) if (!Guid.isValid(e)) console.log(errors.keys);
+            break;
+         }
+         case "obj": {
+            if (typeof Reflect.get(arg, e) !== "object")
+               console.log(errors.isNotObject);
+         }
+      }
+   });
+}
 
 const lock = (rw) => {
    //reid
    if (rw) {
-      fs.chmodSync(db_name, 0o400, (err) => {
-         if (err) throw err;
-      });
+      /*fs.chmodSync(db_name, 0o400);*/
    } else {
       //wraid
-      fs.chmodSync(db_name, 0o200, (err) => {
-         if (err) throw err;
-      });
+      /*fs.chmodSync(db_name, 0o200);*/
    }
 };
 
 const unlock = (rw) => {
    //reid
    if (rw) {
-      fs.chmodSync(db_name, 0o4, (err) => {
-         if (err) throw err;
-      });
+      // fs.chmodSync(db_name, 0o4);
    } else {
       //wraid
-      fs.chmodSync(db_name, 0o2, (err) => {
-         if (err) throw err;
-      });
+      //fs.chmodSync(db_name, 0o2);
    }
+   /*   console.log("usuario asignar");
+   fs.chownSync(db_name, 1541, 999);
+
+   fs.chmodSync(db_name, 0o4);
+   console.log("usuario 0o4");
+   fs.chmodSync(db_name, 0o2);
+   console.log("usuario 0o2");
+   fs.chmodSync(db_name, 0o1);
+   console.log("usuario 0o1");
+*/
 };
 
-function set(res, obj) {
+function set(callback, obj) {
    unlock(false);
 
-   error(res, obj, "  ");
+   argumentsCheck({ callback: callback, obj: obj });
 
-   const ob = {};
+   createFileDb((allData) => {
+      function isSetAllData(getObjAllData) {
+         if (Reflect.set(allData, obj.constructor.name, getObjAllData)) {
+            throw errors.notAdd;
+         }
+      }
 
-   function json(allvalue) {
-      let exe = true;
+      if (allData !== undefined) {
+         let getObjAllData = Reflect.get(allData, obj.constructor.name);
 
-      let objProperty = JSON.parse(
-         JSON.stringify(obj).replace(
-            `"class_data_name":"${obj.class_data_name}",`,
-            " "
-         )
-      );
-
-      if (allvalue !== undefined) {
-         let specifyObj = Reflect.get(allvalue, obj.class_data_name);
-
-         if (specifyObj !== undefined) {
-            const isArray = Array.isArray(specifyObj);
-
-            if (isArray) {
-               specifyObj.push(objProperty);
+         if (getObjAllData !== undefined) {
+            if (Array.isArray(getObjAllData)) {
+               getObjAllData.push(obj);
             } else {
-               const aux = specifyObj;
-               specifyObj = [];
-               specifyObj.push(aux);
-               specifyObj.push(objProperty);
-               console.log("n2: " + JSON.stringify(specifyObj));
+               const aux = getObjAllData;
+               getObjAllData = [];
+               getObjAllData.push(aux);
+               getObjAllData.push(obj);
             }
 
-            exe = Reflect.set(allvalue, obj.class_data_name, specifyObj);
+            isSetAllData(getObjAllData);
          } else {
-            exe = Reflect.set(allvalue, obj.class_data_name, objProperty);
+            isSetAllData(objgetObjAllData);
          }
       } else {
-         allvalue = {};
-         exe = Reflect.set(allvalue, obj.class_data_name, objProperty);
+         isSetAllData(getObjAllData);
       }
 
-      if (exe) {
-         if (allvalue !== undefined) {
-            fs.writeFile(db_name, JSON.stringify(allvalue), (err) => {
-               value = "success";
-               if (err) {
-                  console.log(err);
-                  value = err;
-               }
-               res.json(value);
-            });
-         } else {
-            res.json("not data error.");
-         }
-      } else {
-         res.json("not add data error.");
+      if (allData === undefined) {
+         throw errors.notData;
       }
-   }
 
-   ob.json = json;
-
-   createFileDb(ob);
+      fs.writeFile(db_name, JSON.stringify(allData), (err) => {
+         if (err) throw err;
+      });
+   });
 
    lock(false);
 }
 
-function setKey(res, obj, key) {
+function setUp(callback, obj) {
    unlock(false);
 
-   error(res, obj, key);
+   argumentsCheck({ callback: callback, obj: obj });
 
-   const ob = {};
-
-   function json(allvalue) {
-      let exe = true;
-
-      let objProperty = JSON.parse(
-         JSON.stringify(obj).replace(
-            `"class_data_name":"${obj.class_data_name}",`,
-            " "
-         )
-      );
-
-      if (allvalue !== undefined) {
-         let specifyObj = Reflect.get(allvalue, obj.class_data_name);
-
-         if (specifyObj !== undefined) {
-            const isArray = Array.isArray(specifyObj);
-
-            if (isArray) {
-               specifyObj.push(objProperty);
-            } else {
-               const aux = specifyObj;
-               specifyObj = [];
-               specifyObj.push(aux);
-               specifyObj.push(objProperty);
-               console.log("n2: " + JSON.stringify(specifyObj));
-            }
-
-            exe = Reflect.set(allvalue, obj.class_data_name, specifyObj);
-         } else {
-            exe = Reflect.set(allvalue, obj.class_data_name, objProperty);
+   function replaceEleOfArray() {
+      for (const e in getObjAllData) {
+         if (getObjAllData[e].key == obj.key) {
+            getObjAllData[e] = obj;
+            break;
          }
-      } else {
-         allvalue = {};
-         exe = Reflect.set(allvalue, obj.class_data_name, objProperty);
-      }
-
-      if (exe) {
-         if (allvalue !== undefined) {
-            fs.writeFile(db_name, JSON.stringify(allvalue), (err) => {
-               value = "success";
-               if (err) {
-                  console.log(err);
-                  value = err;
-               }
-               res.json(value);
-            });
-         } else {
-            res.json("not data error.");
-         }
-      } else {
-         res.json("not add data error.");
       }
    }
 
-   ob.json = json;
+   getAll((allData) => {
+      function isSetAllData(getObjAllData) {
+         if (Reflect.set(allData, obj.constructor.name, getObjAllData)) {
+            throw errors.notAdd;
+         }
+      }
 
-   createFileDb(ob);
+      if (allData !== undefined) {
+         let getObjAllData = Reflect.get(allData, obj.constructor.name);
+
+         if (getObjAllData !== undefined) {
+            if (Array.isArray(getObjAllData)) {
+               replaceEleOfArray(getObjAllData);
+            } else {
+               const aux = getObjAllData;
+               getObjAllData = [];
+               getObjAllData.push(aux);
+               replaceEleOfArray(obj);
+            }
+
+            isSetAllData(getObjAllData);
+         } else {
+            isSetAllData(getObjAllData);
+         }
+      } else {
+         isSetAllData(getObjAllData);
+      }
+
+      if (allData !== undefined) {
+         throw errors.notData;
+      }
+
+      fs.writeFile(db_name, JSON.stringify(allData), (err) => {
+         if (err) callback(err);
+      });
+   });
 
    lock(false);
 }
 
-function getAll(res) {
+function getAll(callback) {
    unlock(true);
 
-   error(res, new String(" "), new String(" "));
+   argumentsCheck({ callback: callback });
 
    fs.readFile(db_name, (err, data) => {
       if (err) {
-         console.log(err);
-      } else {
-         res.json(JSON.parse(data));
+         throw err;
       }
+
+      callback(JSON.parse(data));
    });
 
    lock(true);
 }
 
-function get(res, obj) {
+function get(callback, obj) {
    unlock(true);
 
-   error(res, obj, " ");
+   argumentsCheck({ callback: callback, obj: obj });
 
    fs.readFile(db_name, (err, data) => {
       if (err) {
-         console.log(err);
-      } else {
-         res.json(Reflect.get(JSON.parse(data), obj));
+         throw err;
       }
+
+      callback(Reflect.get(JSON.parse(data), obj.constructor.name));
    });
 
    lock(true);
 }
 
-function getKey(res, obj, key) {
+function getKey(callback, obj, key) {
    unlock(true);
 
-   error(res, obj, key);
+   argumentsCheck({ callback: callback, obj: obj, key, key });
 
    fs.readFile(db_name, (err, data) => {
       if (err) {
-         console.log(err);
-      } else {
-         const value = Reflect.get(JSON.parse(data), obj);
+         throw err;
+      }
+      const value = Reflect.get(JSON.parse(data), obj.constructor.name);
 
-         if (key !== undefined) {
-            for (var arr in value) {
-               if (value[arr].key === key) {
-                  res.json(value[arr]);
-                  break;
-               }
-            }
+      for (var arr in value) {
+         if (value[arr].key === key) {
+            callback(value[arr]);
+            break;
          }
       }
    });
@@ -265,10 +244,9 @@ function getKey(res, obj, key) {
    lock(true);
 }
 
-daj.Class = Class;
-daj.get = get;
-daj.getAll = getAll;
-daj.getKey = getKey;
-daj.set = set;
-
-module.exports = daj;
+module.exports.get = get;
+module.exports.getAll = getAll;
+module.exports.getKey = getKey;
+module.exports.set = set;
+module.exports.setUp = setUp;
+module.exports.daj = daj;
